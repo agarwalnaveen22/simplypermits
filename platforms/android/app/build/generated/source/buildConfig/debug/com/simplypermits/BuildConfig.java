434a89399eb31e8334997edc7fc26dcee1fa6a24
Naveen Agarwal <naveen@fonbnk.com>
2022-09-17 15:38:11 +0530

    pushed build

--- platforms/android/app/build/generated/source/buildConfig/debug/com/simplypermits/BuildConfig.java
+++ platforms/android/app/build/generated/source/buildConfig/debug/com/simplypermits/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.simplypermits";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 20010;
-  public static final String VERSION_NAME = "5.0.0";
+  public static final int VERSION_CODE = 20011;
+  public static final String VERSION_NAME = "5.0.2";
 }
